a6fdeb526f3995de3f5eeacae2ef06570c6ba2cf
Justin Law <jlawhome08@gmail.com>
2022-04-03 11:28:29 -0700

    Moved platform sprite behavior into new script
    for refactoring and preventing a bug in which all sprites in the level chunk are considered a platform

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using System.Linq;
 using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour
@@ -8,15 +7,9 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject[] ChunkPrefabs;
     public float chunkHeight = 30f;
     public float spawnOffset = 60f;
-    public float platformFreezeDistance;
-
-    [SerializeField] Sprite frozenGrassSprite;
-    [SerializeField] Sprite freshGrassSprite;
 
     Vector2 currentOffset = Vector2.zero;
     Queue<GameObject> chunkQueue = new Queue<GameObject>();
-
-    public List<SpriteRenderer> platformSprites = new List<SpriteRenderer>();
 
     void Update()
     {
@@ -24,23 +17,6 @@ public class LevelGenerator : MonoBehaviour
         {
             SpawnNextChunk();
         }
-
-        foreach (SpriteRenderer sprite in platformSprites)
-		{
-            if (sprite != null)
-			{
-                sprite.gameObject.transform.localScale = Vector3.one;
-                var collider = sprite.GetComponent<BoxCollider2D>();
-                collider.size = new Vector2(3, 1);
-                if (sprite.transform.position.y - GameManager.Snow.transform.position.y < platformFreezeDistance)
-                {
-                    sprite.sprite = frozenGrassSprite;
-                }
-                else
-                    sprite.sprite = freshGrassSprite;
-            }
-            
-		}
     }
 
     void SpawnNextChunk()
@@ -49,7 +25,6 @@ public class LevelGenerator : MonoBehaviour
         var instance = Instantiate(chunk, currentOffset, Quaternion.identity, transform);
         currentOffset.y += chunkHeight;
         chunkQueue.Enqueue(instance);
-        ResetActiveSprites();
         if (chunkQueue.Count > 3) DespawnFirstChunk();
     }
 
@@ -57,13 +32,5 @@ public class LevelGenerator : MonoBehaviour
     {
         var firstChunk = chunkQueue.Dequeue();
         Destroy(firstChunk);
-        ResetActiveSprites();
-	}
-
-	void ResetActiveSprites()
-	{
-        var tempSprites = GetComponentsInChildren<SpriteRenderer>();
-        platformSprites = tempSprites.ToList();
-	}
-
+    }
 }
