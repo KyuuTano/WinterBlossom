6e4d6d8bb8dad5aa63a3b75412edebe26ecc1c42
tr <jettzeong@gmail.com>
2022-04-03 22:18:33 +0800

    Uploaded sprites to placeholders

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class LevelGenerator : MonoBehaviour
@@ -7,9 +9,14 @@ public class LevelGenerator : MonoBehaviour
     [SerializeField] GameObject[] ChunkPrefabs;
     public float chunkHeight = 30f;
     public float spawnOffset = 60f;
+    [SerializeField] private float snowDistance;
     
     Vector2 currentOffset = Vector2.zero;
     Queue<GameObject> chunkQueue = new Queue<GameObject>();
+    [SerializeField] private List<SpriteRenderer> platformSprites;
+
+    [SerializeField] Sprite frozenGrassSprite;
+    [SerializeField] Sprite freshGrassSprite;
 
     void Update()
     {
@@ -17,14 +24,33 @@ public class LevelGenerator : MonoBehaviour
         {
             SpawnNextChunk();
         }
+
+        foreach (var sprite in platformSprites)
+		{
+            if (sprite != null)
+			{
+                sprite.gameObject.transform.localScale = Vector3.one;
+                var platformCollider = sprite.gameObject.GetComponent<BoxCollider2D>();
+                platformCollider.size = new Vector2(3, 1);
+                if (sprite.transform.position.y - GameManager.Snow.transform.position.y < snowDistance)
+                {
+                    sprite.sprite = frozenGrassSprite;
+                }
+                else
+                    sprite.sprite = freshGrassSprite;
+            }
+		}    
     }
 
     void SpawnNextChunk()
     {
-        var chunk = ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length)];
+        var chunk = ChunkPrefabs[UnityEngine.Random.Range(0, ChunkPrefabs.Length)];
         var instance = Instantiate(chunk, currentOffset, Quaternion.identity, transform);
         currentOffset.y += chunkHeight;
         chunkQueue.Enqueue(instance);
+
+        UpdateSpriteList();
+
         if (chunkQueue.Count > 3) DespawnFirstChunk();
     }
 
@@ -32,5 +58,12 @@ public class LevelGenerator : MonoBehaviour
     {
         var firstChunk = chunkQueue.Dequeue();
         Destroy(firstChunk);
+        UpdateSpriteList();
+    }
+
+	private void UpdateSpriteList()
+	{
+        var tempSpriteArray = GetComponentsInChildren<SpriteRenderer>();
+        platformSprites = tempSpriteArray.ToList();
     }
 }
