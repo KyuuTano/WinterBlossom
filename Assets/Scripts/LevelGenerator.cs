7539f05dc6c06ea09930fc9477cc403252cee2a9
Koi <102782408+koi-2@users.noreply.github.com>
2022-04-04 17:20:54 -0500

    Add initial chunks to level gen

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 public class LevelGenerator : MonoBehaviour
 {
     [SerializeField] Transform player;
-    [SerializeField] GameObject[] ChunkPrefabs;
+    [SerializeField] GameObject[] InitialChunkPrefabs;
+    [SerializeField] GameObject[] RandomChunkPrefabs;
     public float chunkHeight = 30f;
     public float spawnOffset = 60f;
 
     Vector2 currentOffset = Vector2.zero;
     Queue<GameObject> chunkQueue = new Queue<GameObject>();
+    int chunkIndex = 0;
 
     void Update()
     {
@@ -21,7 +23,12 @@ public class LevelGenerator : MonoBehaviour
 
     void SpawnNextChunk()
     {
-        var chunk = ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length)];
+        var chunk = chunkIndex < InitialChunkPrefabs.Length ?
+            InitialChunkPrefabs[chunkIndex] : 
+            RandomChunkPrefabs[Random.Range(0, RandomChunkPrefabs.Length)];
+        
+        chunkIndex += 1;
+
         var instance = Instantiate(chunk, currentOffset, Quaternion.identity, transform);
         currentOffset.y += chunkHeight;
         chunkQueue.Enqueue(instance);
