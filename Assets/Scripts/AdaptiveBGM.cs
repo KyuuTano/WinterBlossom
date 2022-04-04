0d074f12ce7d43c47f315c424fa5d6f16cf62eb5
Justin Law <jlawhome08@gmail.com>
2022-04-03 22:09:18 -0700

    Volume Sliders and Ambience

--- Assets/Scripts/AdaptiveBGM.cs
+++ Assets/Scripts/AdaptiveBGM.cs
@@ -8,20 +8,12 @@ public class AdaptiveBGM : MonoBehaviour
     [SerializeField] FMODUnity.StudioEventEmitter emitter;
     [SerializeField] float distanceToFreeze = 5f;
     [SerializeField] Transform playerTransform;
-    [SerializeField] GameManager gm;
-    [SerializeField] GameObject deathMusicPlayer;
 
     void Update()
     {
         var distance = playerTransform.position.y - GameManager.Snow.transform.position.y;
-        value = 1 - Mathf.Min(distance / distanceToFreeze, 1f);
+        value = 1 - Mathf.Clamp(distance / distanceToFreeze, 0f, 1f);
         emitter.SetParameter(parameterName, value);
-
-        if (gm.IsGameOver)
-        {
-            gameObject.SetActive(false);
-            deathMusicPlayer.SetActive(true);
-        }
     }
 
 
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,13 +18,16 @@ public class GameManager : MonoBehaviour
 
     public GameObject gameOverPanel;
     public GameObject playerObject;
-    
+
     private Camera cam;
+
+    [SerializeField] GameObject GameMusic;
+    [SerializeField] GameObject DeathMusic;
 
     void Start()
     {
         cam = Camera.main;
-       
+
     }
 
     void Update()
@@ -36,11 +39,11 @@ public class GameManager : MonoBehaviour
             GameOver();
         }
         if (!IsGameOver)
-		{
+        {
             timeStart += Time.deltaTime;
             timeText.text = "Time: " + timeStart.ToString("F2");
         }
-        
+
     }
 
     public void GameOver()
@@ -51,26 +54,28 @@ public class GameManager : MonoBehaviour
         Debug.Log("Game over!");
         playerObject.SetActive(false);
         gameOverPanel.SetActive(true);
+        GameMusic.SetActive(false);
+        DeathMusic.SetActive(true);
     }
 
     public void OnRetryClicked()
-	{
+    {
         ResetGame();
-	}
+    }
 
     public void OnMainMenuClicked()
-	{
+    {
         SceneManager.LoadScene("Title");
-	}
-    
+    }
+
     private void ResetGame()
-	{
+    {
         SceneManager.LoadScene("Main");
 
         //IsGameOver = false;
         //gameOverPanel.SetActive(false);
         //playerObject.gameObject.SetActive(true);
         //timeStart = 0;
-        
+
     }
 }
