19fdea5c1c7e46b8c8fd69bfe255d2a48d98dfb7
Justin Law <jlawhome08@gmail.com>
2022-04-03 21:00:07 -0700

    Game Loop and Death Music Plays

--- Assets/Scripts/AdaptiveBGM.cs
+++ Assets/Scripts/AdaptiveBGM.cs
@@ -2,12 +2,27 @@ using UnityEngine;
 
 public class AdaptiveBGM : MonoBehaviour
 {
+    [SerializeField] string parameterName;
     [Range(0f, 1f)]
-    [SerializeField] float parameter1 = 0;
+    [SerializeField] float value;
     [SerializeField] FMODUnity.StudioEventEmitter emitter;
+    [SerializeField] float distanceToFreeze = 5f;
+    [SerializeField] Transform playerTransform;
+    [SerializeField] GameManager gm;
+    [SerializeField] GameObject deathMusicPlayer;
 
     void Update()
     {
-        emitter.SetParameter("Parameter 1", parameter1);
+        var distance = playerTransform.position.y - GameManager.Snow.transform.position.y;
+        value = 1 - Mathf.Min(distance / distanceToFreeze, 1f);
+        emitter.SetParameter(parameterName, value);
+
+        if (gm.IsGameOver)
+        {
+            gameObject.SetActive(false);
+            deathMusicPlayer.SetActive(true);
+        }
     }
+
+
 }
